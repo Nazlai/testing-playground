ea328ac7ec9dc3532e81c85e89c9f5ed769340c7
Joe Ho <joe.he@redso.com.hk>
2021-08-22 13:32:52 +0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+
+const webpackConfig = {
+  mode: "development",
+  entry: "/src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index.bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".scss"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.ts|tsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+  },
+  devtool: "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Janken",
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
+
+module.exports = webpackConfig;
