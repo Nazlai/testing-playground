c4b8f50749b014bc475fecce2ae008f601f1dfcb
Joe Ho <joe.he@redso.com.hk>
2021-09-02 17:07:37 +0800

    add jest-dom

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   moduleNameMapper: {
     "^@screen(.*)$": "<rootDir>/src/screen$1",
   },
+  setupFilesAfterEnv: [
+    "@testing-library/jest-dom",
+    "regenerator-runtime/runtime",
+  ],
   testEnvironment: "jsdom",
 };
